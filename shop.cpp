d89f1443eeaf855b64b64fc0718cbe64f8d9ddf4
sv6-UCC <65443362+sv6-UCC@users.noreply.github.com>
2025-03-19 12:10:00 +0000

    Added Shopping Cart feature
    Can now add games to shopping cart and view what's in shopping cart

--- shop.cpp
+++ shop.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include "user.cpp"
 
 using namespace std;
@@ -9,6 +10,8 @@ void show_menu() {
     vector<user> listOfUsers = {
         {"admin", "password", true, false}
     };
+
+    vector<string> shoppingCart = {};
 
     while (true) {
         int option;
@@ -51,7 +54,8 @@ void show_menu() {
 
                 while (true) {
                     cout << "1. Send a donation\n";
-                    cout << "2. Log Out\n";
+                    cout << "2. View shopping cart\n";
+                    cout << "3. Log Out\n";
                     int user_choice;
                     cin >> user_choice;
 
@@ -60,6 +64,11 @@ void show_menu() {
                             cout << "Thank you for your donation!\n\n";
                             break;
                         case 2:
+                            for (string item : shoppingCart) {
+                                cout << item << "\n";
+                            }
+                            break;
+                        case 3:
                             cout << "You are now logged out!\n\n";
                             goto MainMenu;
                         default:
@@ -85,7 +94,8 @@ void show_menu() {
 
                     while (true) {
                         cout << "1. Send a donation\n";
-                        cout << "2. Log Out\n";
+                        cout << "2. View shopping cart\n";
+                        cout << "3. Log Out\n";
                         int user_choice;
                         cin >> user_choice;
 
@@ -94,6 +104,11 @@ void show_menu() {
                                 cout << "Thank you for your donation!\n\n";
                                 break;
                             case 2:
+                                for (string item : shoppingCart) {
+                                    cout << item << "\n";
+                                }
+                                break;
+                            case 3:
                                 cout << "You are now logged out!\n\n";
                                 goto MainMenu;
                             default:
@@ -118,11 +133,76 @@ void show_menu() {
                 if (choice == 6) break;
 
                 switch (choice) {
-                    case 1: cout << "RPG Games: The Last of Us, Zelda, Walking Dead\n"; break;
-                    case 2: cout << "Action Games: Assassin's Creed, God of War, Resident Evil\n"; break;
-                    case 3: cout << "Retro Games: Tetris, Pac-Man, Pong\n"; break;
-                    case 4: cout << "Puzzle Games: Tetris, Pac-Man, Pong\n"; break;
-                    case 5: cout << "Sports Games: FIFA, NHL, NBA\n"; break;
+                    case 1: 
+                    cout << "RPG Games: 1- The Last of Us, 2-Zelda, 3-Walking Dead\n";
+                    cout << "Which game would you like to add to your shopping cart? (Type 4 to go back):\n";
+                    int rpg_choice;
+                    cin >> rpg_choice;
+                    if (rpg_choice == 4) break;
+                    
+                    switch (rpg_choice) {
+                        case 1: shoppingCart.push_back("The Last of Us"); break;
+                        case 2: shoppingCart.push_back("Zelda"); break;
+                        case 3: shoppingCart.push_back("Walking Dead"); break;
+                        default: cout << "Invalid option. Try again.\n";
+                    }
+                    break;
+                    case 2: 
+                    cout << "Action Games: 1- Assassin's Creed, 2- God of War, 3- Resident Evil\n";
+                    cout << "Which game would you like to add to your shopping cart? (Type 4 to go back):\n";
+                    int action_choice;
+                    cin >> action_choice;
+                    if (action_choice == 4) break;
+
+                    switch (action_choice) {
+                        case 1: shoppingCart.push_back("Assassin's Creed"); break;
+                        case 2: shoppingCart.push_back("God of War"); break;
+                        case 3: shoppingCart.push_back("Resident Evil"); break;
+                        default: cout << "Invalid option. Try again.\n";
+                    }
+                    break;
+                    case 3: 
+                    cout << "Retro Games: 1- Tetris, 2- Pac-Man, 3- Pong\n";
+                    cout << "Which game would you like to add to your shopping cart? (Type 4 to go back):\n";
+                    int retro_choice;
+                    cin >> retro_choice;
+                    if (retro_choice == 4) break;
+
+                    switch (retro_choice) {
+                        case 1: shoppingCart.push_back("Tetris"); break;
+                        case 2: shoppingCart.push_back("Pac-Man"); break;
+                        case 3: shoppingCart.push_back("Pong"); break;
+                        default: cout << "Invalid option. Try again.\n";
+                    }
+                    break;
+                    case 4: 
+                    cout << "Puzzle Games: 1- Tetris, 2- Pac-Man, 3- Pong\n";
+                    cout << "Which game would you like to add to your shopping cart? (Type 4 to go back):\n";
+                    int puzzle_choice;
+                    cin >> puzzle_choice;
+                    if (puzzle_choice == 4) break;
+
+                    switch (puzzle_choice) {
+                        case 1: shoppingCart.push_back("Tetris"); break;
+                        case 2: shoppingCart.push_back("Pac-Man"); break;
+                        case 3: shoppingCart.push_back("Pong"); break;
+                        default: cout << "Invalid option. Try again.\n";
+                    }
+                    break;
+                    case 5: 
+                    cout << "Sports Games: 1- FIFA, 2- NHL, 3- NBA\n";
+                    cout << "Which game would you like to add to your shopping cart? (Type 4 to go back):\n";
+                    int sports_choice;
+                    cin >> sports_choice;
+                    if (sports_choice == 4) break;
+
+                    switch (sports_choice) {
+                        case 1: shoppingCart.push_back("FIFA"); break;
+                        case 2: shoppingCart.push_back("NHL"); break;
+                        case 3: shoppingCart.push_back("NBA"); break;
+                        default: cout << "Invalid option. Try again.\n";
+                    }
+                    break;
                     default: cout << "Invalid option. Try again.\n";
                 }
             }
